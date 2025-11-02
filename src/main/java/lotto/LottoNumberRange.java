54f2352704199ff49ac90d962b071154e4e1e661
conycomy <imgyeong4936@gmail.com>
2025-11-03 03:10:09 +0900

    feat: 로또 번호 범위 및 상수(MIN, MAX, 개수 등) 정의

---
+++ src/main/java/lotto/LottoNumberRange.java
@@ -0,0 +1,12 @@
+package lotto;
+
+public final class LottoNumberRange {
+
+    static final int MIN_NUMBER = 1;
+    static final int MAX_NUMBER = 45;
+    static final int NUMBERS_PER_TICKET = 6;
+    private static final int PRICE_TICKET = 1000;
+
+
+
+}
