191af7eca456f50333dfe3a463e298659a6bc7c8
conycomy <imgyeong4936@gmail.com>
2025-11-03 17:27:31 +0900

    refactor: 상수 접근 제어자를 public static final로 변경

--- src/main/java/lotto/LottoNumberRange.java
+++ src/main/java/lotto/LottoNumberRange.java
@@ -2,11 +2,8 @@ package lotto;
 
 public final class LottoNumberRange {
 
-    static final int MIN_NUMBER = 1;
-    static final int MAX_NUMBER = 45;
-    static final int NUMBERS_PER_TICKET = 6;
-    private static final int PRICE_TICKET = 1000;
-
-
-
+   public static final int MIN_NUMBER = 1;
+   public static final int MAX_NUMBER = 45;
+   public  static final int NUMBERS_PER_TICKET = 6;
+   public static final int PRICE_TICKET = 1000;
 }
