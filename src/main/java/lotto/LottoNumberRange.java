7cfb786d53526cdd020c059627a0622cae3b7b54
conycomy <imgyeong4936@gmail.com>
2025-11-03 23:15:41 +0900

    fix: LottoNumberRange의 중복 상수 정의 제거 및 정리

--- src/main/java/lotto/LottoNumberRange.java
+++ src/main/java/lotto/LottoNumberRange.java
@@ -2,8 +2,12 @@ package lotto;
 
 public final class LottoNumberRange {
 
-   public static final int MIN_NUMBER = 1;
-   public static final int MAX_NUMBER = 45;
-   public  static final int NUMBERS_PER_TICKET = 6;
-   public static final int PRICE_TICKET = 1000;
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 45;
+    public static final int NUMBERS_PER_TICKET = 6;
+    public static final int PRICE_TICKET = 1000;
+
+    private LottoNumberRange() {
+
+    }
 }
